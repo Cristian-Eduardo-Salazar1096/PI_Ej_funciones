4f688c52f4090903ca8636b000b0c801b57b8bb3
Cristian-Eduardo-Salazar1096 <164533888+Cristian-Eduardo-Salazar1096@users.noreply.github.com>
2025-02-07 16:48:59 +0000

    otra vez

--- funcion1.dart
+++ funcion1.dart
@@ -12,9 +12,11 @@ void escribeNombre(){
 void main(){
  //Cristian Salazar Mat:22308051281096
  //llamado a la funcion saludar
+
  print("Cristian Salazar Mat:22308051281096");
  print("llamado a la funcion saludar");
  saludar();
  print("llamado a la funcion escribeNombre");
  escribeNombre();
+ 
  }
