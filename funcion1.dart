7d1a45a4a3960c9659a17331b434ae5c967414d4
Cristian-Eduardo-Salazar1096 <164533888+Cristian-Eduardo-Salazar1096@users.noreply.github.com>
2025-02-07 16:47:43 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+//funcion que no devuelve valor
+//funcion saludar
+
+void saludar(){
+ print("hola mundo");
+ }
+
+void escribeNombre(){
+ print("Cristian Salazar");
+ }
+
+void main(){
+ //Cristian Salazar Mat:22308051281096
+ //llamado a la funcion saludar
+ print("Cristian Salazar Mat:22308051281096");
+ print("llamado a la funcion saludar");
+ saludar();
+ print("llamado a la funcion escribeNombre");
+ escribeNombre();
+ }
