7ae3f2e954548fecd255024a55853d61675a23dc
Cristian-Eduardo-Salazar1096 <164533888+Cristian-Eduardo-Salazar1096@users.noreply.github.com>
2025-02-10 18:04:25 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+//funciones creadas por el usuario
+void suma(int a, int b) {
+  print("La suma es: ${a + b}");
+}
+
+void resta(int a, int b) {
+  print("La resta es: ${a - b}");
+}
+//funcion main
+void main() {
+  print ("Cristian Salazar Mat: 22308051281096");
+  print("Llamando a la funcion suma y resta");
+  suma(10, 5);
+  resta(10, 5);
+}
